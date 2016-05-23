f5ef589abdc242367113a44d51fc2423c9d0d2cb
Florian von Bock <f@vonbock.info>
2016-05-23 20:51:42 +0900

    initial commit

--- bitmask.go
+++ bitmask.go
@@ -1,6 +1,8 @@
 package bitmask
 
 import (
+	"fmt"
+
 	"github.com/fvbock/uds-go/set"
 )
 
@@ -20,15 +22,15 @@ func Bitmask(value int) (b *bitmask) {
 	return
 }
 
-func (b *bitmask) IsSet(value int) {
+func (b *bitmask) IsSet(value int) bool {
 	return b.values.HasMember(value)
 }
 
 func (b *bitmask) setValues() {
 	i := 1
-	for i <= value {
-		if i&f == i {
-			b.values.Add(f)
+	for i <= b.int {
+		if i&b.int == i {
+			b.values.Add(i)
 		}
 		i = i * 2
 	}
@@ -37,5 +39,5 @@ func (b *bitmask) setValues() {
 }
 
 func (b *bitmask) String() string {
-	return fmt.Springf("%v", b.values.Members())
+	return fmt.Sprintf("%v", b.values.Members())
 }
